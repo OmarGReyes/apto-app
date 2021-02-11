8550fd4b948aa7a7177adea28f8c58c285fbd920
Omar González Reyes <omargreyes@hotmail.es>
2021-02-11 13:15:34 -0500

    Connecting to atlas

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/apartamentos',{
+// mongoose.connect('mongodb://localhost/apartamentos',{
+mongoose.connect('mongodb+srv://Coconutella10:Coconutella10@cluster0.md5x3.mongodb.net/apartamentos',{
 // mongoose.connect('mongodb+srv://omargreyes:Coconutella@cluster0.wrghu.mongodb.net/notes-app',{
     useCreateIndex:true, //Obligatoria
     useNewUrlParser:true, //Obligatoria
