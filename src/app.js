f053053770ee68a622df4bb1727096f043fdb175
Omar González Reyes <omargreyes@hotmail.es>
2021-03-26 16:00:24 -0500

    Deleting unused folders

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 import express from 'express'
 import morgan from 'morgan'
-import path from 'path'
-import exphbs from 'express-handlebars'
+
 import _handlebars from 'handlebars'
-import helpers from 'handlebars-helpers'
-import methodOverride from "method-override"
+
 import {allowInsecurePrototypeAccess} from '@handlebars/allow-prototype-access'
 import authRoutes from './routes/auth.routes'
-import userRoutes from './routes/user.routes'
+
 import {createRoles} from './libs/inicialSetup'
 import usersRoutes from './routes/users.routes'
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,4 @@ const router = express.Router()
 router.get('/', (req,res)=>{
     res.render('index')
 })
-
-
-
-
 module.exports=router;
