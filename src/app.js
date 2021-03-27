20c39d9325539078bfc6149964a6832d91fc04bb
Omar González Reyes <omargreyes@hotmail.es>
2021-03-26 21:00:39 -0500

    Moving from dev to dependencies

--- src/app.js
+++ src/app.js
@@ -3,22 +3,17 @@ import morgan from 'morgan'
 
 import _handlebars from 'handlebars'
 
-import {allowInsecurePrototypeAccess} from '@handlebars/allow-prototype-access'
-import authRoutes from './routes/auth.routes'
 
-import {createRoles} from './libs/inicialSetup'
-import usersRoutes from './routes/users.routes'
 
 
 
 const app = express();
-createRoles();
 app.use(express.json())
 app.use(morgan('dev'))
 
-app.use('/api/auth', authRoutes)
+
 // app.use('/api/user', userRoutes)
-app.use('/users', usersRoutes)
+
 //Routes
 app.use(require("./routes/aptos"));
 app.use(require("./routes/index"));
