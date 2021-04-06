ba3b29d68a5a84ae59ca4e591a4860ad3925f07b
Omar González Reyes <omargreyes@hotmail.es>
2021-04-06 09:38:13 -0500

    universal origin URL

--- src/public/js/pagination.js
+++ src/public/js/pagination.js
@@ -11,18 +11,22 @@ anterior.addEventListener('click', anteriorPag)
 function siguientePag(e){
     e.preventDefault()
     let actual = new URLSearchParams(window.location.search)
+    let origin = window.location.origin;
+    console.log(origin);
     console.log(actual);
     let page = Number(actual.get('page'))+1
     const min = actual.get('min')
     const max = actual.get('max')
     const hab = actual.get('hab')
-    window.location.replace(`https://thawing-ravine-09830.herokuapp.com/aptos/filter?min=${min}&max=${max}&hab=${hab}&page=${page}`)
+    window.location.replace(`${origin}/aptos/filter?min=${min}&max=${max}&hab=${hab}&page=${page}`)
     console.log(myParam);    
 }
 
 function anteriorPag(e){
     e.preventDefault()
     let actual = new URLSearchParams(window.location.search)
+    let origin = window.location.origin;
+    console.log(origin);
     let page = Number(actual.get('page'))-1
     const min = actual.get('min')
     const max = actual.get('max')
@@ -30,5 +34,5 @@ function anteriorPag(e){
     if (page ===0){
         return
     }
-    window.location.replace(`https://thawing-ravine-09830.herokuapp.com/aptos/filter?min=${min}&max=${max}&hab=${hab}&page=${page}`)
+    window.location.replace(`${origin}/aptos/filter?min=${min}&max=${max}&hab=${hab}&page=${page}`)
 }
