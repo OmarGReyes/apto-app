de7f1504fbf965f1a04972db54a222ee08d413f5
Omar González Reyes <omargreyes@hotmail.es>
2021-04-07 22:11:15 -0500

    fixing max prize alert bug

--- src/public/js/inputs.js
+++ src/public/js/inputs.js
@@ -8,14 +8,16 @@ const minMax =  (e) =>{
 
     let min = document.querySelector('#min').value
     let max = document.querySelector('#max').value
-
-    if (min > max){
-        e.preventDefault()
-        window.alert("El precio mínimo debe ser mayor al precio máximo")
-        
+    console.log(min)
+    console.log(max)  
+    
+    if (max != 0){
+        if (min > max){
+            e.preventDefault()
+            window.alert("El precio mínimo debe ser menor al precio máximo")        
+        }
     }
 
-    console.log(min)
-    console.log(max)    
+      
 }
 cargarEventListeners();
